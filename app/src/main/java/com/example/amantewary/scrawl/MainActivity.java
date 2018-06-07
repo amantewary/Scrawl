6decf43189f4c5d02d55422bd323760f9fbb4b6a
Nikhil Kamath <nikhilvk91@gmail.com>
2018-06-07 15:19:27 -0300

    Gradle break test

--- app/src/main/java/com/example/amantewary/scrawl/MainActivity.java
+++ app/src/main/java/com/example/amantewary/scrawl/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
