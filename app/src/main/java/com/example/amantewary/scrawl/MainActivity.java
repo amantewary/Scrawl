35c1554449ea42040ca7fc9fa5894fe857e2d581
Aman Tewary <aman.tewary@live.com>
2018-06-19 13:01:09 -0300

    minor fix

--- app/src/main/java/com/example/amantewary/scrawl/MainActivity.java
+++ app/src/main/java/com/example/amantewary/scrawl/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab_add_note);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
