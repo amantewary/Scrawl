d2f38810155f3cf99e5b72425af6917f4f76a049
Aman Tewary <aman.tewary@live.com>
2018-06-21 14:35:07 -0300

    Editied Manifest File

--- app/src/main/java/com/example/amantewary/scrawl/MainActivity.java
+++ app/src/main/java/com/example/amantewary/scrawl/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.amantewary.scrawl;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,8 +27,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), EditNotesActivity.class);
+                startActivity(intent);
             }
         });
 
