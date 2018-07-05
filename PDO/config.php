70ed62c8655c2539b672430d1f43eed188bf61b9
Nikhil Kamath <nikhilvk91@gmail.com>
2018-07-05 09:38:15 -0300

    Add PDO Files

---
+++ PDO/config.php
@@ -0,0 +1,25 @@
+<?php
+$host = 'db-5308.cs.dal.ca';
+$db = 'CSCI5308_19_DEVINT';
+$user = 'CSCI5308_19_DEVINT_USER';
+$pass = 'CSCI5308_19_DEVINT_19248';
+
+
+
+try {
+    $dsn = "mysql:host=$host;dbname=$db;";
+    $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+    $pdo = new PDO($dsn, $user, $pass, $opt);
+
+
+    $status = $pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS);
+
+} catch (PDOException $exception) {
+    die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+}
+
+?>
