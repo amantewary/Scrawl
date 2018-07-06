488369555949aaab42e4a406e327b8e0542c0d24
Aman Tewary <aman.tewary@live.com>
2018-07-06 20:26:13 -0300

    Added Label Model

--- NoteApi/includes/ConnectDb.php
+++ NoteApi/includes/ConnectDb.php
@@ -1,24 +1,19 @@
 <?php 
-	
-
-	class ConnectDb
-	{
-		private $con;
-		function __construct()
-		{
-	 
-		}
-		function connect()
-		{
-			
-		try{
-				include_once dirname(__FILE__) . '/Constants.php';
-				$this->con = new PDO(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-				$this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch(PDOException $e) {
-			echo 'Connection Error: ' . $e->getMessage();
-		}
-			return $this->con;
-		}
-	 
-	}
+  class ConnectDb {
+    private $host = 'db-5308.cs.dal.ca';
+    private $db_name = 'CSCI5308_19_DEVINT';
+    private $username = 'CSCI5308_19_DEVINT_USER';
+    private $password = 'CSCI5308_19_DEVINT_19248';
+    private $conn;
+    
+    public function connect() {
+      $this->conn = null;
+      try { 
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+      }
+      return $this->conn;
+    }
+  }
