53eeefe20d1c1b20ba5ad62cab25561cfb07ea3b
Aman Tewary <aman.tewary@live.com>
2018-07-07 22:19:55 -0300

    Added Create Note Feature To The App

--- app/src/main/java/com/example/amantewary/scrawl/API/INoteAPI.java
+++ app/src/main/java/com/example/amantewary/scrawl/API/INoteAPI.java
@@ -5,8 +5,7 @@ import com.example.amantewary.scrawl.Handlers.NoteHandler;
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
@@ -14,15 +13,8 @@ public interface INoteAPI {
     @GET("read")
     Call<List<NoteHandler>> getNotes();
 
-    @FormUrlEncoded
     @POST("create")
-    Call<NoteHandler> createNote(
-            @Field("label_id") Integer label_id,
-            @Field("title") String title,
-            @Field("body") String body,
-            @Field("url") String url,
-            @Field("author_id") Integer author_id
-    );
+    Call<NoteHandler> createNote(@Body NoteHandler note);
 
 
 }
