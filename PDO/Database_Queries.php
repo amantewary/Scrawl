dbff84794c18709326fb8fe23643cfcb415177ca
Nikhil Kamath <nikhilvk91@gmail.com>
2018-07-12 10:38:55 -0300

    Beautify Logger and Tracker

--- PDO/Database_Queries.php
+++ PDO/Database_Queries.php
@@ -13,33 +13,21 @@ function getUserData($pdo)
 {
 
 
-    try{
+    try {
         $stmt = $pdo->prepare('Select * from user') or die("failed" . mysql_error());
-    $stmt->execute();
+        error_log("\r\nConnection Opened at ". date("d-m-Y (D) H:i:s", time()). "\r\n" , 3, "log.txt");
 
-    $json = json_encode($rows = $stmt->fetchAll(PDO::FETCH_ASSOC));
-    echo $json;
-    }catch(PDOException $e){
+        $stmt->execute();
+
+        $json = json_encode($rows = $stmt->fetchAll(PDO::FETCH_ASSOC));
+        echo $json;
+    } catch (PDOException $e) {
         print_r($e);
-        error_log("Error ". $e, 3, "log.txt");
+        error_log("Time: ".date("d-m-Y (D) H:i:s", time()) ." Error " . $e, 3, "log.txt");
     }
-    
 
 
 }
-
-//try {
-//
-//    $name = "test";
-//    $email = "test@test.com";
-//    $password = "123456";
-////    registerUser($pdo, $name, $email, $password);
-//    loginUser($pdo, $email, $password);
-//
-//} catch (PDOException $ex) {
-//    die(json_decode($ex));
-//}
-
 
 // code to register the user
 function registerUser($pdo, $name, $email, $password)
@@ -51,68 +39,72 @@ function registerUser($pdo, $name, $email, $password)
         $salt = $hash["salt"];
         $stmt = $pdo->prepare("INSERT INTO user (unique_user_id, username , email_address , password , created_at , modified_at , salt )
 VALUES (?, ?, ?, ?, now(), now(), ?)") or die(mysql_error());
+        error_log("\r\nConnection Opened at ". date("d-m-Y (D) H:i:s", time()). "\r\n" , 3, "log.txt");
+
         $stmt->execute(array($uniqueId, $name, $email, $encrypted_password, $salt));
 
         if ($stmt) {
             $stmt = $pdo->prepare("SELECT username,email_address from user where email_address=?");
             $stmt->execute(array($email));
             $rows = $stmt->fetch(PDO::FETCH_ASSOC);
-            $stmt = null;
-            $pdo = null;
+
             return $rows;
         }
     } catch (PDOException $e) {
         print_r($e);
-        error_log("Error ". $e, 3, "log.txt");
+        error_log("Time: ".date("d-m-Y (D) H:i:s", time()) ." Error " . $e, 3, "log.txt");
         return false;
     }
 
 }
 
 
-function loginUser($pdo, $email, $password){
+function loginUser($pdo, $email, $password)
+{
 
-    try{
+    try {
         $stmt = $pdo->prepare("SELECT * from user where email_address=?");
+        error_log("\r\nConnection Opened at ". date("d-m-Y (D) H:i:s", time()). "\r\n" , 3, "log.txt");
+
         $stmt->execute(array($email));
-        if($stmt){
+        if ($stmt) {
             $rows = $stmt->fetch(PDO::FETCH_ASSOC);
             $encrypted_password = $rows['password'];
             $salt = $rows['salt'];
 
-            if ($encrypted_password == dehashPassword($password,$salt)){
-                $stmt = null;
-                $pdo = null;
+            if ($encrypted_password == dehashPassword($password, $salt)) {
+
                 return $rows;
-            }else{
+            } else {
                 return false;
             }
         }
-    }catch (Exception $e){
+    } catch (Exception $e) {
         print_r($e);
-        error_log("Error ". $e, 3, "log.txt");
+        error_log("Time: ".date("d-m-Y (D) H:i:s", time()) ." Error " . $e, 3, "log.txt");
         return false;
     }
 
 }
 
 //check if user exists or not?
-function isUserExists($pdo, $email){
+function isUserExists($pdo, $email)
+{
     $affected_rows = 0;
-    try{
+    try {
         $stmt = $pdo->prepare("SELECT * from user where email_address=?");
+        error_log("\r\nConnection Opened at ". date("d-m-Y (D) H:i:s", time()). "\r\n" , 3, "log.txt");
+
         $stmt->execute(array($email));
         $affected_rows = $stmt->rowCount();
-        if($affected_rows>0){
-            $stmt = null;
-            $pdo = null;
+        if ($affected_rows > 0) {
             return true;
-        }else{
+        } else {
             return false;
         }
-    }catch (Exception $e){
+    } catch (Exception $e) {
         print_r($e);
-        error_log("Error ". $e, 3, "log.txt");
+        error_log("Time: ".date("d-m-Y (D) H:i:s", time()) ." Error " . $e, 3, "log.txt");
         return false;
     }
 }
@@ -132,10 +124,18 @@ function hashPassword($password)
     return $hash;
 }
 
-function dehashPassword($password, $salt){
+function dehashPassword($password, $salt)
+{
     $hash = base64_encode(sha1($password . $salt, true) . $salt);
-
     return $hash;
+}
+
+function closeConnection()
+{
+    $stmt = null;
+    $pdo = null;
+    error_log("Connection Closed at " . date("d-m-Y (D) H:i:s", time()), 3, "log.txt");
+
 }
 
 ?>
