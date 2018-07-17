eb02f93183191ca646f190dc96e1f3d12aec5274
Aman Tewary <aman.tewary@live.com>
2018-07-17 11:20:43 -0300

    Modified Note API for retrieving notes by user_id

--- NoteApi/app/includes/Notes.php
+++ NoteApi/app/includes/Notes.php
@@ -114,6 +114,27 @@ class Notes
         }
     }
 
+    public function readByUser()
+    {
+        error_log('Invoked readByLabel Method');
+        try {
+            $query = 'CALL spGetNoteByUser(:user_id)';
+            $stmt = $this->con->prepare($query);
+            $this->user_id = htmlspecialchars(strip_tags($this->user_id));
+            $stmt->bindParam(':user_id', $this->user_id);
+            if($stmt->execute()) {
+                error_log('Retrieved Note');
+                return $stmt;
+            }else{
+                throw new PDOException();
+            }
+        } catch (\PDOException $e) {
+            print_r($e);
+            error_log('Note Not Available: ' . $e->getMessage());
+            return $e;
+        }
+    }
+
     public function update()
     {
         $query = 'CALL spUpdateNote(:label_name, :title, :body, :url, :user_id, :id)';
