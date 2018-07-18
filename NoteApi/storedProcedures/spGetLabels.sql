4093019970520e7a7b05f9daca8d7159a0960eae
Aman Tewary <aman.tewary@live.com>
2018-07-18 10:52:20 -0300

    Added new stored procedures

--- NoteApi/storedProcedures/spGetLabels.sql
+++ NoteApi/storedProcedures/spGetLabels.sql
@@ -1,4 +1,6 @@
-CREATE DEFINER=`CSCI5308_19_DEVINT_USER`@`%` PROCEDURE `spGetLabels`()
+CREATE DEFINER=`CSCI5308_19_DEVINT_USER`@`%` PROCEDURE `spGetLabels`(IN parameter_user_id INT)
 BEGIN
-	SELECT id, name, created_at FROM labels ORDER BY created_at DESC;
+	SELECT * FROM labels 
+    WHERE user_id IN (parameter_user_id, 0)
+    ORDER BY created_at DESC;
 END
