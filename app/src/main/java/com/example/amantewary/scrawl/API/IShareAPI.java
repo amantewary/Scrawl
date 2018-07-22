84eda38157802524f6f9951c9f2e1a2273a26525
Haofan <haofan.hou@gmail.com>
2018-07-21 23:23:10 -0300

    Split interface for Interface Segregation

--- app/src/main/java/com/example/amantewary/scrawl/API/IShareAPI.java
+++ app/src/main/java/com/example/amantewary/scrawl/API/IShareAPI.java
@@ -1,24 +1,14 @@
 package com.example.amantewary.scrawl.API;
 
 import com.example.amantewary.scrawl.Handlers.ShareHandler;
-import com.example.amantewary.scrawl.Handlers.UserClass;
 
-import java.util.Map;
-
-import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Multipart;
 import retrofit2.http.POST;
-import retrofit2.http.PartMap;
 
 public interface IShareAPI {
 
     @POST("~hhou/QA_Devint/NoteApi/v1/shares/create")
     Call<ShareHandler> createShare(@Body ShareHandler share);
-
-    @Multipart
-    @POST("~hhou/QA_Devint/checkIfUserExist.php")
-    Call<UserClass> checkIfUserExists(@PartMap Map<String, RequestBody> parameters);
 
 }
--- app/src/main/java/com/example/amantewary/scrawl/ViewNotesActivity.java
+++ app/src/main/java/com/example/amantewary/scrawl/ViewNotesActivity.java
@@ -21,6 +21,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.amantewary.scrawl.API.IShareAPI;
+import com.example.amantewary.scrawl.API.Users.ICheckUser;
 import com.example.amantewary.scrawl.Handlers.UserClass;
 import com.example.amantewary.scrawl.API.Notes.INoteResponse;
 
@@ -119,16 +120,12 @@ public class ViewNotesActivity extends AppCompatActivity implements View.OnClick
             }
         });
 
-
-
-
         btn_edit.setOnClickListener(this);
         btn_collaborate.setOnClickListener(this);
         btn_share.setOnClickListener(this);
         btn_delete.setOnClickListener(this);
 
         sessionManager = new SessionManager(getApplicationContext());
-
     }
 
     @Override
@@ -166,7 +163,6 @@ public class ViewNotesActivity extends AppCompatActivity implements View.OnClick
         } catch (Exception e) {
             Log.e(TAG, "Message: " + e.toString());
         }
-
     }
 
     @Override
@@ -272,7 +268,7 @@ public class ViewNotesActivity extends AppCompatActivity implements View.OnClick
                 final String share_from = sessionManager.getUserEmail();
 
                 final Boolean[] result = new Boolean[1];
-                IShareAPI service = RetroFitInstance.getRetrofit().create(IShareAPI.class);
+                ICheckUser service = RetroFitInstance.getRetrofit().create(ICheckUser.class);
                 RequestBody body = RequestBody.create(MediaType.parse("text/plain"), collaborate_with);
                 Map<String, RequestBody> requestBodyMap = new HashMap<>();
                 requestBodyMap.put("email", body);
